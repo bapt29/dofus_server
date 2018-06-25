65d43b43b207c22e4dcb161c62eb0ab9b97b1f73
Baptiste <baptiste.cabiten@gmail.com>
2018-06-25 14:50:51 +0200

    initial commit

--- database/models/guild.py
+++ database/models/guild.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, SmallInteger, BigInteger
+from sqlalchemy.orm import relationship
 
 from database.core.base import Base
 
@@ -8,6 +9,8 @@ class Guild(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
+
+    members = relationship('GuildMember', )
 
     def __init__(self, name):
         self.name = name
